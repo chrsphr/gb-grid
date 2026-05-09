b8227cc60b4de4a89303ab8c4c0c85301681019d
Chris McNeill <chris@mcneill.fyi>
2026-05-09 21:10:37 +0100

    updated dependencies

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "GB power grid energy database (BMRS Insights ingester)";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
